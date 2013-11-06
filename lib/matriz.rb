ed30767bc3d0e139b8ca2226a844b0bcfe9146fa
Daniel Luis Martin <alu0100537093@ull.edu.es>
2013-11-06 00:51:10 +0000

    Creadas y cumplidas las expectativas de los operadores suma, resta y ==.

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,6 +1,7 @@
+
 class Matriz
 
-	attr_reader :fil, :col, :matriz
+	attr_reader :fil, :col, :matrix
 
 	def initialize(array)
 		
@@ -10,5 +11,50 @@ class Matriz
 		
 	
 	end
+	
+	
+	
+	
+	def +(other) #Sobrecarga del operador + para sumar dos matrices
+		
+		copia = @matrix
+		for i in (0...@fil)
+			for j in (0...@col)
+				copia[i][j] = copia[i][j] + other.matrix[i][j]
+			end
+		end
+	
+	Matriz.new(copia)
+	
+	end
+				
+
+	def -(other) #Sobrecarga del operador - para restar dos matrices
+		
+		copia = @matrix
+		for i in (0...@fil)
+			for j in (0...@col)
+				copia[i][j] = copia[i][j] - other.matrix[i][j]
+			end
+		end
+	
+	Matriz.new(copia)
+	
+	end
+
+	def ==(other) #Sobrecarga del operador == para poder comprobar si dos matrices son iguales
+	
+		if @matrix == other.matrix
+		
+			true
+		
+		else
+			
+			false
+		
+		end
+	
+	end
+
 
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -4,17 +4,21 @@ describe Matriz do
 
 	before :each do
 
-	@m1=Matriz.new([2,5,3],[3,1,4],[1,2,3])
+	@m1=Matriz.new([[2,5,3],[3,1,4],[1,2,3]])
 
 	end
 
 
 	it "Se suma correctamente 2 matrices con el operador +" do
 
-	(@m1 + Matriz.new([3,4,5],[5,3,2],[3,4,5])) == Matriz.new([5,9,8],[8,4,6],[4,6,8])
+	(@m1 + Matriz.new([[3,4,5],[5,3,2],[3,4,5]])) == Matriz.new([[5,9,8],[8,4,6],[4,6,8]])
 
 	end
 
-	
+	it "Se restan correctamente 2 matrices con el operador -" do
+
+	(@m1 - Matriz.new([[1,4,2],[2,1,2],[1,0,1]])) == Matriz.new([[1,1,1],[1,0,2],[0,2,2]])
+
+	end
 
 end
